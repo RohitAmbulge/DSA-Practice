22807936cc9a29963294cae63934d4a82dcc9a10
RohitAmbulge <rohitambulge15@gmail.com>
2025-07-30 17:55:37 +0530

    update gcd

--- Array/Math/FindGCD.java
+++ Array/Math/FindGCD.java
@@ -1,24 +1,15 @@
 public class FindGCD {
 
     public static int print(int n1, int n2) {
-        int max = 0;
-        int min = 0;
+        int gcd = 1;
+        for (int i = 1; i <= Math.min(n1, n2); i++) {
 
-        if (n1 < n2) {
-            min = n1;
-            max = n2;
-        } else {
-            min = n2;
-            max = n1;
-        }
-
-        for (int i = min; i >= 1; i--) {
-            if (max % i == 0 && min % i == 0) {
-                return i;
+            if (n1 % i == 0 && n2 % i == 0) {
+                gcd = i;
             }
         }
 
-        return -1;
+        return gcd;
     }
 
     public static void main(String[] args) {
