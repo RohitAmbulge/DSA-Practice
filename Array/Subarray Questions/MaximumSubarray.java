f6785922b62ec2e4616e34951cb59d945ac098bb
RohitAmbulge <rohitambulge15@gmail.com>
2025-08-05 19:06:29 +0530

    Leetcode Q.53

---
+++ Array/Subarray Questions/MaximumSubarray.java
@@ -0,0 +1,27 @@
+public class MaximumSubarray {
+
+    private static int maxSubArray(int[] nums) {
+
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (sum > maxSum)
+                maxSum = sum;
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        // int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        int[] nums = { 5, 4, -1, 7, 8 };
+        System.out.println(maxSubArray(nums));
+    }
+}
