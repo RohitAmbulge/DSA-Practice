b22b73fe5656197f6961a7e3eed941b32b9a9a34
RohitAmbulge <rohitambulge15@gmail.com>
2025-08-07 19:29:25 +0530

    update code

--- Array/Subarray Questions/LongestSubarrayWithGivenSumK.java
+++ Array/Subarray Questions/LongestSubarrayWithGivenSumK.java
@@ -1,6 +1,9 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class LongestSubarrayWithGivenSumK {
 
-    public static int longestSum(int[] arr, int n, int k) {
+    public static int longestSum(int[] arr, int k) {
         int maxLen = Integer.MIN_VALUE;
 
         for (int i = 0; i < arr.length; i++) {
@@ -16,13 +19,38 @@ public class LongestSubarrayWithGivenSumK {
         return maxLen;
     }
 
+    public static int longestSumUsingMap(int arr[], int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int preSum = 0;
+        int maxLen = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            preSum += arr[i];
+
+            if (preSum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            if (map.containsKey(preSum - k)) {
+                maxLen = Math.max(maxLen, i - map.get(preSum - k));
+            }
+
+            if (!map.containsKey(preSum)) {
+                map.put(preSum, i);
+            }
+        }
+
+        return maxLen;
+
+    }
+
     public static void main(String[] args) {
 
-        int n = 9;
-        int k = 10;
-        int[] arr = { 2, 3, 5, 1, 9 };
+        int k = 5;
+        int[] arr = { 2, 3, 5 };
 
-        System.out.println(longestSum(arr, n, k));
+        System.out.println(longestSum(arr, k));
 
     }
 }
