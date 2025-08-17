5cb18bfd06410e53a127ca4393b1298fb682fe3d
RohitAmbulge <rohitambulge15@gmail.com>
2025-08-17 16:10:12 +0530

    update

---
+++ Recursion/GFG_Rec_Practice/SumOfNaturalNumber.java
@@ -0,0 +1,15 @@
+public class SumOfNaturalNumber {
+
+    public static int findSum(int n) {
+        if (n == 0)
+            return 0;
+
+        return n + findSum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println(findSum(n));
+    }
+}
